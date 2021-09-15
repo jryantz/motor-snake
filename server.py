c2ddc42b01d67284b73a4055bd60c1dbcb997f06
Jonathan Yantz <jon@motorlabs.io>
2021-09-15 14:00:46 -0400

    Updated battlesnake info

--- server.py
+++ server.py
@@ -24,10 +24,10 @@ def handle_info():
     print("INFO")
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "jryantz",  # TODO: Your Battlesnake Username
+        "color": "#007AFD",  # TODO: Personalize
+        "head": "evil",  # TODO: Personalize
+        "tail": "hook",  # TODO: Personalize
     }
 
 
